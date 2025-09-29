0e46df8684bbef75a6b881e1276717fbd9bbc484
alessandro negrin <alessandro.negrin@gmail.com>
2025-09-29 22:50:52 +0200

    optmizing flash write (single page erase) (#32)

--- firmware/ledsStripController/Core/Src/diskio.c
+++ firmware/ledsStripController/Core/Src/diskio.c
@@ -13,8 +13,10 @@
 
 /* Example: Declarations of the platform and disk functions in the project */
 #include <string.h>
+#include <stdlib.h>
 
 #define PAGE_SIZE 2048
+#define SECTORS_PER_PAGE PAGE_SIZE / FF_MIN_SS
 
 /*-----------------------------------------------------------------------*/
 /* Get Drive Status                                                      */
@@ -62,6 +64,27 @@ DRESULT disk_read(
 
 #if FF_FS_READONLY == 0
 
+typedef struct FlashPage
+{
+    uint32_t page_start;
+    LBA_t sectors[SECTORS_PER_PAGE];
+    uint32_t sector_data_index[SECTORS_PER_PAGE];
+    struct FlashPage *next;
+} FlashPage;
+
+FlashPage* new_flash_page()
+{
+    FlashPage *fp = (FlashPage *)malloc(sizeof(FlashPage));
+    for (int j = 0; j < SECTORS_PER_PAGE; j++)
+    {
+        fp->sectors[j] = 0;
+        fp->sector_data_index[j] = 0;
+    }
+    fp->page_start = 0;
+    fp->next = NULL;
+    return fp;
+}
+
 uint32_t find_sector_start(LBA_t sector) {
     return USB_FLASH_START_ADDRESS + (sector * FF_MIN_SS);
 } 
@@ -81,36 +104,68 @@ DRESULT disk_write(
 {
     HAL_StatusTypeDef ret = HAL_OK;
 
+    // build operations
+    FlashPage *fp = NULL;
+    FlashPage *fp_head = NULL;
     for (int i = 0; i < count; i++)
     {
+        LBA_t current_sector = sector + i;
+        uint32_t page_address = find_page_start(current_sector);
+        uint32_t sector_address = find_sector_start(current_sector);
+        uint8_t sector_relative_pos = (sector_address - page_address) / FF_MIN_SS;
 
-        uint8_t data[FF_MIN_SS];
+        if (fp == NULL)
+        {
+            fp = new_flash_page();
+            fp_head = fp;
+        }
+        else
+        {
+            if (fp->page_start != page_address)
+            {
+                FlashPage *new_fp = new_flash_page();
+                fp->next = new_fp;
+                fp = new_fp;
+            }
+        }
 
-        memcpy(data, &buff[i * FF_MIN_SS], FF_MIN_SS);
+        fp->page_start = page_address;
+        fp->sectors[sector_relative_pos] = sector_address;
+        fp->sector_data_index[sector_relative_pos] = i * FF_MIN_SS;
+    }
 
+    FlashPage *iter = fp_head;
+    while (iter)
+    {
         ret = HAL_FLASH_Unlock();
         if (ret != HAL_OK)
         {
-            return RES_NOTRDY;
+            ret = RES_NOTRDY;
+            break;
         }
 
-        LBA_t current_sector = sector + i;
-        uint32_t pageAddress = find_page_start(current_sector);
-
         uint8_t page_data[PAGE_SIZE];
-        memcpy(page_data, (const void *)pageAddress, PAGE_SIZE);
-        memcpy(&page_data[find_sector_start(current_sector) - pageAddress], data, FF_MIN_SS);
+        memcpy(page_data, (const void *)(iter->page_start), PAGE_SIZE);
+        for (int i = 0; i<SECTORS_PER_PAGE; i++){
+
+            uint32_t sector_address = iter->sectors[i];
+            if (sector_address) {
+                memcpy(&page_data[i * FF_MIN_SS], &buff[iter->sector_data_index[i]], FF_MIN_SS);
+            }
+
+        }
 
         FLASH_EraseInitTypeDef EraseInitStruct;
         uint32_t SectorError;
         EraseInitStruct.TypeErase = FLASH_TYPEERASE_PAGES;
-        EraseInitStruct.PageAddress = pageAddress;
+        EraseInitStruct.PageAddress = iter->page_start;
         EraseInitStruct.NbPages = 1;
 
         ret = HAL_FLASHEx_Erase(&EraseInitStruct, &SectorError);
         if (ret != HAL_OK)
         {
-            return RES_NOTRDY;
+            ret = RES_NOTRDY;
+            break;
         }
 
         for (uint32_t j = 0; j < PAGE_SIZE / 4; j++)
@@ -118,7 +173,7 @@ DRESULT disk_write(
             uint32_t word;
             memcpy(&word, &page_data[j * 4], 4);
 
-            ret = HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, pageAddress + (j * 4), word);
+            ret = HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, iter->page_start + (j * 4), word);
             if (ret != HAL_OK)
             {
                 break;
@@ -126,6 +181,16 @@ DRESULT disk_write(
         }
 
         HAL_FLASH_Lock();
+
+        iter = iter->next;
+    }
+
+    FlashPage *free_me_iter = fp_head;
+    while (free_me_iter)
+    {
+        FlashPage *temp = free_me_iter;
+        free_me_iter = free_me_iter->next;
+        free(temp);
     }
 
     return ret != HAL_OK ? RES_ERROR : RES_OK;
