97a5dbccee0354adc5b09389d0ab15f0c6ae747e
gaucho1978 <gaucho1978@users.noreply.github.com>
2025-12-20 11:08:11 +0100

    resets usb after hw reset

--- firmware/ledsStripController/USB_DEVICE/App/usb_device.c
+++ firmware/ledsStripController/USB_DEVICE/App/usb_device.c
@@ -37,45 +37,52 @@
 USBD_HandleTypeDef hUsbDeviceFS;
 
 void MX_USB_DEVICE_Init(void){
+
+	// --- Reset USB core --- prevents problems when exiting from a Hardware Reset
+	__HAL_RCC_USB_FORCE_RESET();
+	HAL_Delay(2);
+	__HAL_RCC_USB_RELEASE_RESET();
+	HAL_Delay(10);
+
 	/* Init Device Library, add supported class and start the library. */
 
-	/*commented for test in V.3.0.0a
+	///*commented for test in V.3.0.0a
 	if (USBD_Init(&hUsbDeviceFS, &FS_Desc, DEVICE_FS) != USBD_OK){
 		onboardLed_red_on();
-		onboardLed_blue_on();
+		//onboardLed_blue_on();
 		Error_Handler();
 	}
 
-	*/
+	//*/
 
-	/*commented for test in V.3.0.0a and V.3.0.0b
+	///*commented for test in V.3.0.0a and V.3.0.0b
 	#ifdef ENABLE_USB_MASS_STORAGE
 		if (USBD_RegisterClass(&hUsbDeviceFS, &USBD_MSC) != USBD_OK){
 	#else
 		if (USBD_RegisterClass(&hUsbDeviceFS, &USBD_CDC) != USBD_OK){
 	#endif
 			onboardLed_red_on();
-			onboardLed_blue_on();
+			//onboardLed_blue_on();
 			Error_Handler();
 		}
-	*/
+	//*/
 
-	/*commented for test in V.3.0.0a, V.3.0.0b and V.3.0.0c
+	///*commented for test in V.3.0.0a, V.3.0.0b and V.3.0.0c
 	#ifdef ENABLE_USB_MASS_STORAGE
 		if (USBD_MSC_RegisterStorage(&hUsbDeviceFS, &USBD_Storage_Interface_fops_FS) != USBD_OK){
 	#else
 		if (USBD_CDC_RegisterInterface(&hUsbDeviceFS, &USBD_Interface_fops_FS) != USBD_OK){
 	#endif
 			onboardLed_red_on();
-			onboardLed_blue_on();
+			//onboardLed_blue_on();
 			Error_Handler();
 		}
 
 	if (USBD_Start(&hUsbDeviceFS) != USBD_OK){
 		onboardLed_red_on();
-		onboardLed_blue_on();
+		//onboardLed_blue_on();
 		Error_Handler();
 	}
 
-	*/
+	//*/
 }
